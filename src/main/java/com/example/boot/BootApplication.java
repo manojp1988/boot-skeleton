2ce4db0e598df5539259e40fc2915e4440d26832
manoj p <manojp1988@gmail.com>
2021-03-03 08:20:25 +0530

    Configured to output war file.

--- src/main/java/com/example/boot/BootApplication.java
+++ src/main/java/com/example/boot/BootApplication.java
@@ -2,11 +2,12 @@ package com.example.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class BootApplication {
+public class BootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
